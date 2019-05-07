8a4cd5defdfee17240b75b27b30418c8282b0c36
Irmina Stepien <irm.stepien@gmail.com>
2019-05-07 05:28:52 +0200

    Basic model class added

---
+++ app/src/main/java/com/example/mylibrary/BookModel.java
@@ -0,0 +1,39 @@
+package com.example.mylibrary;
+
+public class BookModel {
+
+    int bookId;
+    String bookTitle, bookAuthor, bookLanguage;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookLanguage() {
+        return bookLanguage;
+    }
+
+    public void setBookLanguage(String bookLanguage) {
+        this.bookLanguage = bookLanguage;
+    }
+}
