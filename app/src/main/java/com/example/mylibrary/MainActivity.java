9d0db79c308d4f1d793c8b5e03c793b577d239b7
Irmina Stepien <irm.stepien@gmail.com>
2019-06-25 14:19:49 +0200

    MainActivity modified

--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -6,24 +6,35 @@ import android.view.View;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.github.clans.fab.FloatingActionButton;
-
 
 public class MainActivity extends AppCompatActivity {
 
-   private FloatingActionButton fabAddNewBook, fabShowAllBooks;
+
+    private FloatingActionButton fabAddNewBook, fabShowAllBooks;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         fabAddNewBook = findViewById(R.id.fabAddNewBook);
         fabAddNewBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AddNewBookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        fabShowAllBooks = findViewById(R.id.fabShowAllBooks);
+        fabShowAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AllBooksActivity.class);
                 startActivity(intent);
             }
         });
