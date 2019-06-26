c1d9b04cd9132134a5d00e36ac3cdf17b93651cf
Irmina Stepien <irm.stepien@gmail.com>
2019-06-26 10:16:11 +0200

    menu_book_options added

--- app/src/main/java/com/example/mylibrary/AddNewBookActivity.java
+++ app/src/main/java/com/example/mylibrary/AddNewBookActivity.java
@@ -1,15 +1,18 @@
-package com.example.mylibrary;
+package com.example.mylibrary.Activity;
 
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mylibrary.BookModel;
+import com.example.mylibrary.BookRoomDatabase;
+import com.example.mylibrary.R;
 
 public class AddNewBookActivity extends AppCompatActivity {
 
